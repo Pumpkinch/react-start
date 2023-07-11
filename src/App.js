19a6df5dbec743badb62520343e9339a8363f6ef
Pumpkinch <melkiyfufonchik@gmail.com>
2023-07-11 13:10:51 +0300

    no logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import WidgetApps from './components/WidgetApps';
+import Popap from './components/Popap';
 
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App({ isEnabled }) {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
 
         <a
           className="App-link"
@@ -17,11 +20,14 @@ function App({ isEnabled }) {
           rel="noopener noreferrer"
         >
           Learn React2345
-        </a>       
+        </a>   
+        <Popap/> 
+
         <WidgetApps />
 
       </header>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Popap/Popap.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+import Close from '../../close.png';
+import Input from '../Input';
+import Button from '../Button/Button';
+
+const Popap = (buttonText, color, sizeBut, line) => {
+    return (
+        <div className='window'>
+            <div className='head'>
+                <p className="headingText">Изменение <br />пароля</p>
+                <button className="close"><img src={Close} /></button>
+            </div>
+
+            <p className="loginText">логин <br /><span>amaxbox.ya</span></p>
+
+            <Input value = 'Текущий пароль' error='Неверный пароль'/>
+            <p className="forgot">Если вы забыли пароль,
+            <a href="#"> восстановите его</a>
+            </p>
+            <hr color="#DDDFE1" size='1'/>
+            <Input value = 'Новый пароль'/>
+            <Input value = 'Повторите пароль' subText='Не используйте пробел и < > % & ;'/>
+            <p className="pastText">После смены пароля вам необходимо заново войти в аккаунт на всех используемых устройствах</p>
+            <Button buttonText= 'продолжить' sizeBut='for-popap' />
+        </div>
+    )
+}
+
+
+export default Popap;
